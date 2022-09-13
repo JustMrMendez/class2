0f797561faf5fdc3ab20ee7f3c5573af5e95d02e
Mr. Mendez <mrmendez@amvestor.com>
2022-09-13 00:49:26 +0000

    + addPost function

--- static/utils.js
+++ static/utils.js
@@ -5,17 +5,19 @@ async function load_posts() {
   const data = await response.json();
 }
 
-async function add_post() {
+async function addPost() {
+    console.log("its working");
   const response = await fetch(
     "https://random-data-api.com/api/v2/users?size=1"
   );
   const data = await response.json();
-  const table = document.getElementById("posts");
-
-  const row = table.insertRow();
-  row.insertCell().innerText = post.first_name;
-  row.insertCell().innerText = post.last_name;
+  const feed = document.querySelector(".feed");
+  const newPost = document.createElement("feed-post");
+  feed.insertBefore(newPost, feed.firstChild);
+  l;
 }
+// const addPostBtn = document.querySelector("#add");
+// addEventListener( "click", this.addDiv.bind( this ) )
 
 window.onscroll = () => {
   if (
